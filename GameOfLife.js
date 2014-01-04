272ece8ea720d33f0ce56b850d9518480473a810
fonzerelly <ChristianHoerauf@web.de>
2014-01-04 23:21:04 +0100

    Conways Game of Life and Testspec so far...

---
+++ GameOfLife.js
@@ -0,0 +1,109 @@
+/*global _, load, print, runCommand*/
+load("lib/lodash.js");
+_.mixin({
+    "containsNonPrimitive": function (collection, nonPrimitiveTarget) {
+        return _.any(collection, _.partial(_.isEqual, nonPrimitiveTarget));
+    },
+
+    "assortedComparator" : function (predicate) {
+        return function (x, y) {
+            if (predicate(x, y)) {
+                return -1;
+            }
+            if (predicate(y, x)) { 
+                return 1;
+            }
+            return 0;
+        };
+    },
+
+    "alikedComparator" : function (predicate) {
+        return function (x, y) {
+            if (predicate(x, y)) {
+                return -1;
+            }
+            return 1;
+        };
+    }
+});
+
+var _defineNeighbors = function (myCell) {
+    var x = myCell[0],
+        y = myCell[1];
+    return [[x-1, y], [x-1,y-1], [x-1,y+1], [x, y-1], [x,y+1], [x+1,y], [x+1,y+1], [x+1,y-1]];
+};
+var _intersection = function(a, b) {
+    return _.filter(a, function(x) {
+        return _.containsNonPrimitive(b, x);
+    });
+};
+
+var simpleCellPredicat = function (cellA, cellB) {
+    var prevailDimension = Number(cellA[0] === cellB[0]);
+    return cellA[prevailDimension] < cellB[prevailDimension]; 
+};
+
+var nextGeneration = function (initField) {
+    var cleanedByDeadCells= _.filter(initField, function (cell) {
+        var neighbors = _defineNeighbors(cell);
+        var result = _intersection(neighbors, initField);
+        return result.length > 1 && result.length <= 3;
+    });
+    var enlivedCells = _.filter(
+        _.filter(
+            _.filter(
+                _.flatten(
+                    _.map(initField, _defineNeighbors),
+                    true
+                ),
+                function (potentialNewCell, index, array) {
+                    var count = _.reduce(
+                       array, 
+                       function (count, aNeighborCell) {
+                           count += Number(_.isEqual(potentialNewCell, aNeighborCell));
+                           return count;
+                       },
+                       0
+                   );
+                   return count===3;
+                }
+            ).sort(_.assortedComparator(simpleCellPredicat)),
+            function(cell, index, array) {
+                return (index % 3) === 0;
+            }
+        ),
+        function(cell) {
+            return !_.containsNonPrimitive(cleanedByDeadCells, cell);
+        }
+    );
+
+    return cleanedByDeadCells.concat(enlivedCells);
+};
+
+var printField = function (cells) {
+    var output = "";
+    _.each(_.range(30), function (y) {
+        var line="";
+        _.each(_.range(30), function (x) {
+            var mask = [" ","X"];
+            line += (mask[Number(_.containsNonPrimitive(cells, [x,y]))]); 
+        });
+        line += "\n";
+        output += line;
+    });
+    print(output);
+};
+
+
+var next=[[19,20], [20,20], [21,20],[20,21],[20,22],[20,23]];
+//printField(nextGeneration(nextGeneration(next)));
+//var newField = nextGeneration(next);
+//var bugField = nextGeneration (newField);
+function main() {
+    while (true) {
+    printField(next);
+    next = nextGeneration(next);
+    runCommand("cmd","/c","cls");
+    }
+}
+main();
---
+++ Spec_GameOfLife.js
@@ -0,0 +1,91 @@
+/*global describe, it, expect, beforeEach,  _, load, print, java, nextGeneration*/
+load("GameOfLife.js");
+/**/
+describe("_.containsNonPrimitive", function () {
+    it ("shall find an Array in an Array", function () {
+        expect(_.containsNonPrimitive([[1,1], [1,2], [1,3]], [1,1])).toEqual(true);
+    });
+});
+describe("nextGeneration", function() {
+    beforeEach(function () {
+        this.addMatchers({
+            toContainNonPrimitive: function (expected) {
+                var actual = this.actual;
+                var notText = this.isNot ? " not ": "";
+                var cascadedArrayToString = function (cascadedArray) {
+                    var arrayString = _.reduce(
+                        cascadedArray,
+                        function (result, arrayElement, index, cascadedArray) {
+                            if (_.isArray(arrayElement)) {
+                                result += cascadedArrayToString(arrayElement);
+                            } else {
+                                result += String(arrayElement);
+                                if (index < (cascadedArray.length-1)) {
+                                    result += ", ";
+                                }
+                            }
+                            return result;
+                        },
+                        "["
+                    ) + "]";
+                    return arrayString;
+                };
+
+                            
+                this.message = function () {
+                    return "Expected " + cascadedArrayToString(actual) + notText + " to contain " + cascadedArrayToString(expected);
+                };
+                return _.containsNonPrimitive(actual, expected);
+            }
+        });
+    });
+
+    it("should stay empty", function() {
+        var next = nextGeneration([]);
+        expect(next).toEqual([]);
+    });
+    it("should die if single living cell", function() {
+        var next = nextGeneration([[0,0]]);
+        expect(next).toEqual([]);
+    });
+    it("should not die if living cell has two living neighbors", function() {
+        var next = nextGeneration([[0,0], [1,1], [2,2]]);
+        expect(next.length).toEqual(1);
+        expect(_.containsNonPrimitive(next,[1,1])).toEqual(true);
+    });
+    it("should die if living cell has more then three neighbours", function() {
+        var next = nextGeneration([[0,0],[1,0],[2,0],[1,1],[1,-1]]);
+        expect(next).not.toContainNonPrimitive([1,0]);
+        expect(next).toContainNonPrimitive([1,-1]);
+        expect(next).toContainNonPrimitive([0,0]);
+        expect(next).toContainNonPrimitive([1,1]);
+        expect(next).toContainNonPrimitive([2,0]);
+    });
+    it("should enlive if surrounded by at exactly three neighbours", function () {
+        var next = nextGeneration([[0,0],[0,2],[2,0]]);
+        expect(next).toContainNonPrimitive([1,1]);
+        expect(next.length).toEqual(1);
+    });
+    it("sould convert shortest cross to square", function () {
+        var next = nextGeneration([[-1,0],[0,-1],[1,0],[0,1],[0,0]]);
+        expect(next).not.toContainNonPrimitive([0,0]);
+        expect(next).toContainNonPrimitive([-1,0]);
+        expect(next).toContainNonPrimitive([-1,-1]);
+        expect(next).toContainNonPrimitive([0,-1]);
+        expect(next).toContainNonPrimitive([1,-1]);
+        expect(next).toContainNonPrimitive([1,0]);
+        expect(next).toContainNonPrimitive([1,1]);
+        expect(next).toContainNonPrimitive([0,1]);
+        expect(next).toContainNonPrimitive([-1,1]);
+    });
+});
+/*
+describe("HashSet", function () {
+    it ("shall store no duplicates", function () {
+        var mySet = new java.util.HashSet();
+        expect(mySet.equals(new java.util.HashSet())).toEqual(true);
+    });
+});
+*/
+/**__Jasmine_been_here_before__"
+) :: failed*/
